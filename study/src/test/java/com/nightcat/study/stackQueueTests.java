d5f096a34db2a6f694bd5c25784944763cf0d419
nightcat <nightcat910@gmail.com>
2020-11-24 17:18:44 +0900

    트럭 문제

--- study/src/test/java/com/nightcat/study/stackQueueTests.java
+++ study/src/test/java/com/nightcat/study/stackQueueTests.java
@@ -2,6 +2,7 @@ package com.nightcat.study;
 
 import com.nightcat.study.stackQueue.Question1;
 import com.nightcat.study.stackQueue.Question2;
+import com.nightcat.study.stackQueue.Question3;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,6 +30,13 @@ class stackQueueTests {
         Assert.assertEquals(q.solution(q.bridge_length2, q.weight2, q.truck_weights2), q.answer2);
         Assert.assertEquals(q.solution(q.bridge_length3, q.weight3, q.truck_weights3), q.answer3);
     }
+
+    @Test
+    void question3(){
+        Question3 q = new Question3();
+        Assert.assertEquals(q.solution(q.priorities1, q.location1), q.answer1);
+    }
+
 
 
 }
